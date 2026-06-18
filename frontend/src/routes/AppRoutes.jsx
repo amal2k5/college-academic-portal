9bcb92e134de3e45e4b402bc750ef28e4ba5d9e0
amal2k5 <amalkrishnapocox2@gmail.com>
2026-06-18 15:53:21 +0530

    Fix authentication routing integration

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import AdminRoutes from "./AdminRoutes";
+import Login from "../pages/admin/auth/Login";
 
 function AppRoutes() {
-  return <AdminRoutes />;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<AdminRoutes />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
