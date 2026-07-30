fce470d2f643445efaf351086e814f1176d1dec2
amal2k5 <amalkrishnapocox2@gmail.com>
2026-07-30 11:00:39 +0530

    Fix FCM service worker import

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -3,12 +3,12 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from accounts.middleware import JWTAuthMiddleware
-from notifications.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from accounts.middleware import JWTAuthMiddleware
+from notifications.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
